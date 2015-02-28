cb1f26ada37c59919f55d4db923fffc27668618b
BBerastegui <BBerastegui@users.noreply.github.com>
2015-02-28 22:34:17 +0100

    Refactor.

---
+++ bashrc
@@ -0,0 +1,17 @@
+alias doIpForward="sudo sysctl -w net.ipv4.ip_forward=1"
+alias undoIpForward="sudo sysctl -w net.ipv4.ip_forward=0"
+alias showIpForward="cat /proc/sys/net/ipv4/ip_forward"
+
+alias gimmeanewmac="sudo ifconfig wlan0 down && sudo macchanger -r wlan0 && sudo ifconfig wlan0 up"
+alias newTor="sudo service tor restart && sudo service polipo restart && sleep 1 && curl -x http://localhost:8118 ip.cow.cat"
+
+export GOPATH=~/src/gocode
+export PATH=$PATH:$GOPATH/bin
+
+export PATH="$PATH:$HOME/.rvm/bin:$HOME/src/go/bin" # Add RVM to PATH for scripting
+
+export EDITOR=vim
+
+function ssht(){
+	ssh $* -t "tmux a || tmux || /bin/bash"
+}
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+install_stuff (){
+# Install stuff
+STUFF_TO_INSTALL="curl nmap tmux"
+sudo apt-get update
+sudo apt-get install $STUFF_TO_INSTALL
+}
+
+config_files (){
+# Backup the .bashrc file
+cp ~/.bashrc ~/.bashrc.old
+# Append the bashrc to the current .bashrc
+cat ./bashrc >> ~/.bashrc
+# Source .bashrc
+source ~/.bashrc
+
+# Backup the .vimrc
+mv ~/.vimrc ~/.vimrc.old
+# Create link to the "good" .vimrc
+ln -s ~/.vimrc .vimrc
+
+# Backup the .tmux.conf
+mv ~/.tmux.conf ~/.tmux.conf.old
+# Create link to the "good" .tmux.conf
+ln -s ~/.tmux.conf .tmux.conf
+
+# Install patogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+}
+
+install_stuff
+config_files
